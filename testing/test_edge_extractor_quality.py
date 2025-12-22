35fed46f1b3119a06aabb8174e9d34f96666b63d
Jesse Sandberg <js2746@viscosus.ccib.rutgers.edu>
2025-12-22 16:24:59 -0500

    added pytest

--- testing/test_edge_extractor_quality.py
+++ testing/test_edge_extractor_quality.py
@@ -16,12 +16,17 @@ from vesicle_edge_extractor.edge_extractor import extract_edge_from_frame
 @pytest.fixture(scope='module')
 def all_sample_videos():
     video_list = []
-    for file in glob.glob('./testing/sample_vesicle_videos/*.npy'):
+    for file in glob.glob('./sample_vesicle_videos/*.npy'):
         video = VesicleVideo(np.load(file))
         video.extract_edges(extract_edge_from_frame)
         video_list.append(video)
-    yield video_list
+    return video_list
+
+
+def test_whether_files_all_saved(all_sample_videos):
+    assert len(all_sample_videos) == 8, "One or more videos failed"
 
 
 def test_whether_edges_extracted(all_sample_videos):
-    assert len(all_sample_videos) == 8, "One or more videos failed"
+    for video in all_sample_videos:
+        assert not np.isnan(video.x_vals).any(), "Some x_vals are nan"
