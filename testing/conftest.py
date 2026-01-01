b53db2f56e8351d5bf74f52aa07d282577d001ae
Jesse Sandberg <jesse@Jesses-MacBook-Air.local>
2025-12-31 20:43:20 -0500

    moved hook 2 to conftest

--- testing/conftest.py
+++ testing/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 def pytest_addoption(parser):
     parser.addoption(
         "--update-ref-values", 
@@ -7,3 +8,24 @@ def pytest_addoption(parser):
         default=False,
         help="Update reference values instead of asserting"
     )
+
+
+def pytest_collection_modifyitems(config, items):
+    """
+    If --update-ref-values flag supplied to pytest, skip everything except the
+    fixture and test you need to update the reference values.
+    """
+    update = config.getoption("--update-ref-values")
+    if not update:
+        return
+    
+    skip_marker = pytest.mark.skip(
+        reason = "Skipped because --update-ref-vales is True"
+    )
+
+    for item in items:
+        if item.nodeid not in [
+            "testing/test_edge_extractor_quality.py::sample_videos",
+            "testing/test_edge_extractor_quality.py::test_extraction_quality",
+        ]:
+            item.add_marker(skip_marker)
